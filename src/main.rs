73e1c7480969764d7ec3368e59d0cbead9044bec
Wladimir Palant <fqcgithub@palant.de>
2022-04-26 06:57:43 +0200

    Turned password generation into a subcommand

--- src/main.rs
+++ src/main.rs
@@ -11,32 +11,43 @@ use rpassword;
 use std::process;
 
 /// PfP: Pain-free Passwords, command line edition
-#[derive(Parser)]
+#[derive(clap::Parser)]
 #[clap(author, version, about, long_about = None)]
 struct Args
 {
-    /// Website name to generate password for
-    domain: String,
-    /// User name associated with the account
-    name: String,
-    /// Password revision
-    #[clap(short = 'r', long, default_value = "1")]
-    revision: String,
-    /// Password length
-    #[clap(short = 'l', long, default_value_t = 16, parse(try_from_str = parse_length))]
-    length: u8,
-    /// Do not include lower-case letters
-    #[clap(short = 'w', long)]
-    no_lower: bool,
-    /// Do not include upper-case letters
-    #[clap(short = 'u', long)]
-    no_upper: bool,
-    /// Do not include digits
-    #[clap(short = 'd', long)]
-    no_digit: bool,
-    /// Do not include symbols
-    #[clap(short = 's', long)]
-    no_symbol: bool,
+    #[clap(subcommand)]
+    command: Commands,
+}
+
+#[derive(clap::Subcommand)]
+enum Commands
+{
+    /// Generate a password
+    Generate
+    {
+        /// Website name to generate password for
+        domain: String,
+        /// User name associated with the account
+        name: String,
+        /// Password revision
+        #[clap(short = 'r', long, default_value = "1")]
+        revision: String,
+        /// Password length
+        #[clap(short = 'l', long, default_value_t = 16, parse(try_from_str = parse_length))]
+        length: u8,
+        /// Do not include lower-case letters
+        #[clap(short = 'w', long)]
+        no_lower: bool,
+        /// Do not include upper-case letters
+        #[clap(short = 'u', long)]
+        no_upper: bool,
+        /// Do not include digits
+        #[clap(short = 'd', long)]
+        no_digit: bool,
+        /// Do not include symbols
+        #[clap(short = 's', long)]
+        no_symbol: bool,
+    }
 }
 
 fn parse_length(arg: &str) -> Result<u8, String>
@@ -60,31 +71,37 @@ fn parse_length(arg: &str) -> Result<u8, String>
 fn main()
 {
     let args = Args::parse();
-    let mut charset = crypto::new_charset();
-    if !args.no_lower
+    match &args.command
     {
-        charset.insert(crypto::CharacterType::LOWER);
-    }
-    if !args.no_upper
-    {
-        charset.insert(crypto::CharacterType::UPPER);
-    }
-    if !args.no_digit
-    {
-        charset.insert(crypto::CharacterType::DIGIT);
-    }
-    if !args.no_symbol
-    {
-        charset.insert(crypto::CharacterType::SYMBOL);
-    }
-    if charset.len() == 0
-    {
-        eprintln!("You need to allow at least one character set.");
-        process::exit(1);
-    }
+        Commands::Generate {domain, name, revision, length, no_lower, no_upper, no_digit, no_symbol} =>
+        {
+            let mut charset = crypto::new_charset();
+            if !no_lower
+            {
+                charset.insert(crypto::CharacterType::LOWER);
+            }
+            if !no_upper
+            {
+                charset.insert(crypto::CharacterType::UPPER);
+            }
+            if !no_digit
+            {
+                charset.insert(crypto::CharacterType::DIGIT);
+            }
+            if !no_symbol
+            {
+                charset.insert(crypto::CharacterType::SYMBOL);
+            }
+            if charset.len() == 0
+            {
+                eprintln!("You need to allow at least one character set.");
+                process::exit(1);
+            }
 
-    let master_password = rpassword::prompt_password("Your master password: ").unwrap();
-    let password = crypto::derive_password(&master_password, &args.domain, &args.name, &args.revision, args.length, charset);
-    println!("Password generated");
-    println!("{}", password);
+            let master_password = rpassword::prompt_password("Your master password: ").unwrap();
+            let password = crypto::derive_password(&master_password, &domain, &name, &revision, *length, charset);
+            println!("Password generated");
+            println!("{}", password);
+        }
+    }
 }
